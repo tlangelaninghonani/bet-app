2d6659b9c8585ba57179cc251821d45ab840bd1c
tlangelaninghonani <nghonanitlangelani772@gmail.com>
2021-12-11 00:54:43 -0800

    changes

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     'google' => [
         'client_id' => "776844087043-j9lvbifsdfgg1d641gqeml6h8j1lql1h.apps.googleusercontent.com",
         'client_secret' => "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",
-        'redirect' => "http://mabuza-app.herokuapp.com0/google_auth_callback"
+        'redirect' => "https://mabuza-app.herokuapp.com/google_auth_callback"
     ],
     'facebook' => [
         'client_id' => '478941016752764',
